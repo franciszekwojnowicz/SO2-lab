87c168c89df905a182e83459d244fb378bf34adb
franciszekwojnowicz <62144431+franciszekwojnowicz@users.noreply.github.com>
2024-05-06 12:43:58 +0200

    part2 for lab2

--- lab2/lab2-2.sh
+++ lab2/lab2-2.sh
@@ -3,6 +3,7 @@
 FIRST_DIR=${1:-dir1}
 SECOND_DIR=${2:-dir2}
 
+#check folders
 if [[ ! -d "${FIRST_DIR}" ]]; then
     echo "Ni ma takiego folderu ${FIRST_DIR}"
     exit 1
@@ -16,11 +17,24 @@ fi
 ITEMS=$(ls "${FIRST_DIR}")
 for ITEM in ${ITEMS}; do
     ext="${ITEM##*.}"
-    if [[ -f  "${FIRST_DIR}/${ITEM}" ]]; then
-        echo "${ITEM} to jest plik regularny"
+    temp_name="${ITEM%.*}"
+    if [[ -L  "${FIRST_DIR}/${ITEM}" ]]; then
+        echo "${ITEM} - to jest dowiazanie symboliczne"
     elif [[ -d "${FIRST_DIR}/${ITEM}" ]]; then
-        echo "${ITEM} to jest katalog"
-    elif [[ -s "${FIRST_DIR}/${ITEM}" ]]; then
-        echo "${ITEM} to jest dowiazanie symboliczne"
+        echo "${ITEM} - to jest katalog" 
+        # extension service   
+        if [[ "${temp_name}" == "${ext}" ]]; then
+            ln -s "../${FIRST_DIR}/${ITEM}" "${SECOND_DIR}/${temp_name^^}_ln"
+        else
+            ln -s "../${FIRST_DIR}/${ITEM}" "${SECOND_DIR}/${temp_name^^}_ln.${ext}"
+        fi
+    elif [[ -f "${FIRST_DIR}/${ITEM}" ]]; then
+        echo "${ITEM} - to jest plik regularny"
+        # extension service   
+        if [[ "${temp_name}" == "${ext}" ]]; then
+            ln -s "../${FIRST_DIR}/${ITEM}" "${SECOND_DIR}/${temp_name^^}_ln"
+        else
+            ln -s "../${FIRST_DIR}/${ITEM}" "${SECOND_DIR}/${temp_name^^}_ln.${ext}"
+        fi
     fi
 done
